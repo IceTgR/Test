30ad99f129a9409106a10d26cacdd3eb19b04d81
IceTgR <icetgr@gmail.com>
2026-02-24 12:43:29 +0100

    Testing out functions

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 import streamlit as st #enable Streamlit
 
 st.write("Hooray, we connected everything")
+st.download_button("Little Git Cheatsheet ;)", https://github.com/IceTgR/Test/blob/master/README.md)
