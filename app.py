a64b13f62a2885575f674c76ee881278d0f44155
IceTgR <icetgr@gmail.com>
2026-02-24 12:50:04 +0100

    Testing out

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st #enable Streamlit
 
 st.write("Hooray, we connected everything")
 
-with open("README.md", "rb") as f:          # oder "r" für Text
+with open("README.md") as f:
     readme_bytes = f.read()
 
 st.download_button(label="Little Git Cheatsheet ;)", data=readme_bytes, file_name="README.md")
