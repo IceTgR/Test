a81c8912c216d91540abba08b302ce05c895c256
IceTgR <icetgr@gmail.com>
2026-02-24 14:11:43 +0100

    Change greeting message to 'Hello World'

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st #enable Streamlit
 
-st.write("Hooray, we connected everything")
+st.write("Hello World")
 
 with open("README.md") as f:
     readme_bytes = f.read()
+
 
 st.download_button(label="Little Git Cheatsheet ;)", data=readme_bytes, file_name="Cheatsheet.md", mime="text/markdown")
