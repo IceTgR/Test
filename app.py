4386cf7202c5b56d95a232495222135d4b2e8d8e
IceTgR <icetgr@gmail.com>
2026-02-24 12:44:42 +0100

    fixing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st #enable Streamlit
 
 st.write("Hooray, we connected everything")
-st.download_button("Little Git Cheatsheet ;)", https://github.com/IceTgR/Test/blob/master/README.md)
+st.download_button("Little Git Cheatsheet ;)", Readme.md)
