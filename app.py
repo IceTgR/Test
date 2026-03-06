b5aed23be91608ca7373db7539df6eecac93053a
IceTgR <icetgr@gmail.com>
2026-03-06 12:25:45 +0100

    Add radio button for team selection

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ st.write("Hello World")
 
 with open("README.md") as f:
     readme_bytes = f.read()
-
-
 st.download_button(label="Little Git Cheatsheet ;)", data=readme_bytes, file_name="Cheatsheet.md", mime="text/markdown")
+
+st.radio("Pick one", ["Liverpool", "Manchester"])
